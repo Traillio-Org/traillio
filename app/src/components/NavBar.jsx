493a6804fa59255839d1122e40e242ca82baf868
theseyan <sayanjyotidasworkmail@gmai.com>
2025-02-15 05:51:05 +0530

    navbar

--- app/src/components/NavBar.jsx
+++ app/src/components/NavBar.jsx
@@ -1,33 +1,33 @@
 import {Home, ChartPie, Cog, Crosshair, UsersRound, LibraryBig} from "lucide-react";
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 export default function() {
     return (
         <div class="navbar">
-            <Link to="/" class="item active">
+            <NavLink to="/" className="item">
                 <Home />
                 <span>Home</span>
-            </Link>
-            <Link to="/analytics" class="item">
+            </NavLink>
+            <NavLink to="/analytics" className="item">
                 <ChartPie />
                 <span>Analytics</span>
-            </Link>
-            <Link to="/courses" class="item">
+            </NavLink>
+            <NavLink to="/courses" className="item">
                 <LibraryBig />
                 <span>Courses</span>
-            </Link>
-            <Link to="/focus" class="item">
+            </NavLink>
+            <NavLink to="/focus" className="item">
                 <Crosshair />
                 <span>Focus</span>
-            </Link>
-            <Link to="/leaderboard" class="item">
+            </NavLink>
+            <NavLink to="/leaderboard" className="item">
                 <UsersRound />
                 <span>Leaderboard</span>
-            </Link>
-            <Link to="/settings" class="item">
+            </NavLink>
+            <NavLink to="/settings" className="item">
                 <Cog />
                 <span>Settings</span>
-            </Link>
+            </NavLink>
         </div>
     );
 }
